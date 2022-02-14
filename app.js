864cf3a447be89d45a2b804993562c0ede33b154
Daniel Cruz <cruzdg@miamioh.edu>
2022-02-14 18:54:19 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({ 
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: "software",
+})
+
+con.connect((err) => {
+    if (err) {
+        console.log(err);
+        console.log("Error in connection");
+        return;
+
+    }
+    console.log("Sucessfully connected");
+});
+let me;
+
+con.query("SELECT * FROM login", (err, rows) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(rows[0].userName);
+    me = rows[0].userName;
+})
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(me);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+con.end((err) => {
+
+});
