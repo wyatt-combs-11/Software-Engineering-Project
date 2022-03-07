caf71ceab25a05852a5c2fb558c2f0fc3c6c69ab
Ryan Holthouse <ryanholthouse@Ryans-MacBook-Pro-9.local>
2022-03-07 15:19:59 -0500

    Made some funny colors. also there's transitions now. Gang gang

--- AllWorkouts.js
+++ AllWorkouts.js
@@ -6,13 +6,33 @@ $(document).ready(function(){
     console.log("JQUERY IS WORKING?");
     
     $("#test").mouseenter(function(){
-        $("#exercise").hide();
-        $("#description").show();
+        $("#exercise").fadeOut(250);
+        setTimeout(function(){
+            $("#description").show();
+        },250);
     })
 
     $("#test").mouseleave(function() {
-        $("#exercise").show();
         $("#description").hide();
+        $("#exercise").fadeIn(250);
     })
 
+    /*
+    $("#lightdarkTog").onclick(function() {
+        var theme = document.getElementsByTagName('link')[0];
+        if (theme.getAttribute('href') == "baseWebsite.css") {
+            theme.setAttribute('href', "baseWebsiteDark.css");
+        } else {
+            theme.setAttribute('href', "baseWebsite.css");
+        }
+    })
+    */
+
+
+
+
+
   });
+
+  
+  
