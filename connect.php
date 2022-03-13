38129982437bb9f49960cdb51bcc6e77b06acf97
Daniel Cruz <cruzdg@miamioh.edu>
2022-03-13 17:21:23 -0400

    test

---
+++ connect.php
@@ -0,0 +1,5 @@
+<?php
+mysql_connect('aws-exercisedb.camvz480jeos.us-east-2.rds.amazonaws.com',
+    'JimPeople', 'Muscles201');
+
+mysql_select_db('exerciseDB');
---
+++ data.php
@@ -0,0 +1,10 @@
+<html>
+    <head>  
+    </head>
+
+    <body>
+        Name: <input type="text" id="name">
+        <div id="name-data"></div>
+        <script src="./jquery/jquery.js"></script>
+        <script src="global.js">
+    </body>
