cd5dee6d35c7b03ba60a3ba74613444c21a101b9
Daniel Cruz <cruzdg@miamioh.edu>
2022-03-18 19:39:49 -0400

    TEST

--- html/AllWorkouts.php
+++ html/AllWorkouts.php
@@ -35,7 +35,15 @@
             <li><a href="PushWorkouts.html">Push Workouts</a></li>
             <li><a href="PullWorkouts.html">Pull Workouts</a></li>
             <li><a href="LegWorkouts.html">Leg Workouts</a></li>
-            <li style=float:right><a href="../Login.php">Log In</a></li>
+            <li style=float:right> <?php
+                if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == true){
+                    echo YOU ARE LOGGED IN
+                } else {
+                    echo LOGIN PLEASE
+                }
+            ?>
+            <li>
+            <!-- <li style=float:right><a href="../Login.php">Log In</a></li> -->
             <li id = "lightdarkTog" style=float:right><a onclick="ColorChange()">Toggle Light/Dark Mode</a></li>
         </ul>
     </div>
