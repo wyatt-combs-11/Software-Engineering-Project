2013124550d36f579e69d9d2d41b9890d2edad10
Daniel Cruz <cruzdg@miamioh.edu>
2022-03-18 20:07:49 -0400

    more testing

--- html/Favorites.php
+++ html/Favorites.php
@@ -44,14 +44,14 @@
             <li><a href="PushWorkouts.html">Push Workouts</a></li>
             <li><a href="PullWorkouts.html">Pull Workouts</a></li>
             <li><a href="LegWorkouts.html">Leg Workouts</a></li>
-            <!-- <li style=float:right> 
-                // if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == true){
-                //     echo YOU ARE LOGGED IN
-                // } else {
-                //     echo LOGIN PLEASE
-                // }
-            
-            <li> -->
+            <li style=float:right> <?php
+                if($_SESSION["loggedin"] == true){
+                    echo htmlspecialchars("LOG OUT")
+                } else {
+                    echo htmlspecialchars("Log In")
+                }
+            ?>
+            <li>
             <li style=float:right><a href="../Login.php">Log In</a></li>
             <li id = "lightdarkTog" style=float:right><a onclick="ColorChange()">Toggle Light/Dark Mode</a></li>
         </ul>
