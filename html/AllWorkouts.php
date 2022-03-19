764c08628ee0b3301470ac3e606bb81f64c50223
Daniel Cruz <cruzdg@miamioh.edu>
2022-03-19 16:34:05 -0400

    TEST

--- html/AllWorkouts.php
+++ html/AllWorkouts.php
@@ -25,12 +25,14 @@
     <?php
         session_start();
 
-        echo "$_SESSION['mode']";
+        //echo "$_SESSION['mode']";
 
         if ($_SESSION["mode"] === 1) {
             echo '<script>ColorChange();</script>';
         }
     ?>
+
+    <script>ColorChange();</script>
 
     <div id="titleDiv">
         <img id = "logo" src="../images/Logo.PNG" alt="Jim People Logo">
