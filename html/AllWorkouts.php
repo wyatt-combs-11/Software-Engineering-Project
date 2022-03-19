5993bfae8c3cfe4a56800602734da1fe50bed6b7
Daniel Cruz <cruzdg@miamioh.edu>
2022-03-19 16:35:43 -0400

    why

--- html/AllWorkouts.php
+++ html/AllWorkouts.php
@@ -24,16 +24,13 @@
 <body>
     <?php
         session_start();
-
-        //echo "$_SESSION['mode']";
+        $test = $_SESSION["mode"];
+        echo "$test";
 
         if ($_SESSION["mode"] === 1) {
             echo '<script>ColorChange();</script>';
         }
     ?>
-
-    <script>ColorChange();</script>
-
     <div id="titleDiv">
         <img id = "logo" src="../images/Logo.PNG" alt="Jim People Logo">
         <h1 class="mainTitle"> All Workouts</h1>
