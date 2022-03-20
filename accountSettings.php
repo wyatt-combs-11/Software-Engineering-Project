a106356589bef0dc86fec2131036cbfdf5688d84
Daniel Cruz <cruzdg@miamioh.edu>
2022-03-20 00:11:30 -0400

    TEST

--- accountSettings.php
+++ accountSettings.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-        header("location: ../Login.php");
+        header("location: ./Login.php");
         exit;
     }
 ?>
